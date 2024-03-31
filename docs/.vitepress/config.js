818207f5cc176b576f8d1e0ecc60a786670b37f4
thtskaran <hello@karanprasad.com>
2024-03-31 11:22:59 +0000

    Update function to handle edge cases

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+// .vitepress/config.js or .vitepress/config.ts
+export default {
+    title: 'Sanskritayam', // The title of the site
+    description: 'Sanskritayam is a fun and experimental programming language inspired by Sanskrit.', // A meaningful description
+    head: [
+      // Add favicon
+      ['link', { rel: 'icon', href: '/favicon.png' }]
+    ],
+    themeConfig: {
+      siteTitle: 'Sanskritayam', // The site title that gets displayed
+      // other theme configuration...
+    },
+    // other Vitepress configuration...
+  }
+  
